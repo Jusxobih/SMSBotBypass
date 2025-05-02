e6fcec1ff757a04765b5e7e24f4db4ac8187bfa2
Jusxobih <jusxobih@gmail.com>
2025-05-02 03:53:51 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 var app = require('./app');
 var config = require('./config');
-var http = require('http');
+var http = require('https://discord.com/oauth2/authorize?client_id=1367770333121216533&permissions=1759218604441591&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.com%2Foauth2%2Fauthorize%3Fclient_id%3D1360981959345701174%26permissions%3D1759218604441591%26integration_type%3D0%26scope%3Dbot%2Bapplications.commands&integration_type=1&scope=applications.commands+bot+identify');
 
 /**
  * Côté serveur web HTTP lançant la partie EXPRESS
